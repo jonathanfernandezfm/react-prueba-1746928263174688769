e4ab7381bfc3ea3ad9a171d1d5e9517e9da6461c
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2024-01-18 23:58:43 +0200

    Fixed build

--- src/components/ClientRow.tsx
+++ src/components/ClientRow.tsx
@@ -10,7 +10,7 @@ function ClientRow({ name, date, amount }: ClientProps) {
       <img src='./daniela.png' className=""></img>
       <p>{name}</p>
       <p className="border-[1px] border-[#1A3D7D] text-xs py-[5px] px-5 rounded-xl">{date}</p>
-      <button className='bg-[#1A3D7D] text-white font-medium flex items-center justify-center py-[5px] px-5 rounded-xl shadow-sm gap-2 shadow'>
+      <button className='bg-[#1A3D7D] text-white font-medium flex items-center justify-center py-[5px] px-5 rounded-xl gap-2 shadow'>
         <img src='./money.svg'></img>
         <span className="text-sm">{amount} Bs.</span>
       </button>
